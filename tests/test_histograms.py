fc0a3d99f4912feb851dec2208474687c052b338
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-03-04 21:29:34 -0700

    Add fixtures to pytest code for histograms

--- tests/test_histograms.py
+++ tests/test_histograms.py
@@ -2,20 +2,36 @@ from eazieda import __version__
 from eazieda.histograms import histograms
 from vega_datasets import data
 import pandas as pd
-from pytest import raises
+from pytest import raises, fixture
 
-df = data.iris()
+
+@fixture
+def df():
+    df = data.iris()
+    return df
+
+
 # Due to Altair bug, need to resample data for second Facet chart for it to not break.
-df_sample = df.sample(df.shape[0], random_state=42)
-features = df.columns
-plot = histograms(df, features, plot_width=100, plot_height=100, num_cols=2)
+@fixture
+def df_sample():
+    df = data.iris()
+    df_sample = df.sample(df.shape[0], random_state=42)
+    return df_sample
+
+
+@fixture
+def plot():
+    df = data.iris()
+    features = df.columns
+    plot = histograms(df, features, plot_width=100, plot_height=100, num_cols=2)
+    return plot
 
 
 def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_histograms_exceptions():
+def test_histograms_exceptions(df):
     with raises(ValueError):
         histograms(
             df,
@@ -42,7 +58,7 @@ def test_histograms_exceptions():
         )
 
 
-def test_histograms_data():
+def test_histograms_data(plot, df, df_sample):
     chart_data = list(plot.to_dict()["datasets"].values())
     pd.testing.assert_frame_equal(df, pd.DataFrame(chart_data[0]))
     pd.testing.assert_frame_equal(
