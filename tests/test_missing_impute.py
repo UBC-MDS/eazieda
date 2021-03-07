8511653416210aa63579c7513a77a78f7d7b9949
vigneshRajakumar <vigneshr.93@gmail.com>
2021-03-06 23:20:29 -0500

    Minor test correction bug fix

--- tests/test_missing_impute.py
+++ tests/test_missing_impute.py
@@ -1,5 +1,5 @@
 from eazieda import __version__
-from eazieda import missing_impute
+from eazieda.missing_impute import missing_impute
 import pandas as pd
 import numpy as np
 from pytest import raises, fixture
@@ -10,35 +10,38 @@ def test_version():
 
 @fixture
 def df_miss():
-    df = pd.DataFrame([[1, "x"], [np.nan, "y"], [2, np.nan], [3, "y"]], columns = ['a', 'b'])
+    df = pd.DataFrame([[1., "x"], [np.nan, "y"], [2., np.nan], [3., "y"]], columns = ['a', 'b'])
     return df
 
 def test_missing_impute(df_miss):
     
     # Test with default arguments
-    expected_output_default = pd.DataFrame(data = {'a': [1, 2, 2, 3], 'b': ['x', 'y', 'y', 'y']})
+    expected_output_default = pd.DataFrame(data = {'a': [1., 2., 2., 3.], 'b': ['x', 'y', 'y', 'y']}).reset_index(drop=True)
     
     missing_output_default = missing_impute(df_miss)
-    
+
     assert pd.DataFrame.equals(missing_output_default, expected_output_default)
     
     # Test with two drop arguments selected at the same time
-    expected_output_two_drop = pd.DataFrame(data = {'a': [1, 3], 'b': ['x', 'y']})
+    expected_output_two_drop = pd.DataFrame(data = {'a': [1., 3.], 'b': ['x', 'y']}).reset_index(drop=True)
     
     missing_output_two_drop = missing_impute(df_miss, method_num="drop", method_non_num="drop")
     
+    print(expected_output_two_drop)
+    print(missing_output_two_drop)
+
     assert pd.DataFrame.equals(missing_output_two_drop, expected_output_two_drop)
     
     
     # Test with only one drop argument is selected
-    expected_output_one_drop = pd.DataFrame(data = {'a': [1, 2, 3], 'b': ['x', 'y', 'y']})
+    expected_output_one_drop = pd.DataFrame(data = {'a': [1., 2., 3.], 'b': ['x', 'y', 'y']}).reset_index(drop=True)
     
     missing_output_one_drop = missing_impute(df_miss, method_non_num="drop")
     
     assert pd.DataFrame.equals(expected_output_one_drop, missing_output_one_drop)
     
     # Test with method_num="median" 
-    expected_output_median = pd.DataFrame(data = {'a': [1, 2, 2, 3], 'b': ['x', 'y', 'y', 'y']})
+    expected_output_median = pd.DataFrame(data = {'a': [1., 2., 2., 3.], 'b': ['x', 'y', 'y', 'y']}).reset_index(drop=True)
     missing_output_median = missing_impute(df_miss, method_num="median")
     
     assert pd.DataFrame.equals(missing_output_median, expected_output_median)
