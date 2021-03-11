2ad5084c8eb442b5d06827aaa6f1c96797748993
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-03-11 16:27:40 -0700

    Fix styling on all files.

--- tests/test_outlier_detect.py
+++ tests/test_outlier_detect.py
@@ -8,19 +8,26 @@ from eazieda.outliers_detect import outliers_detect_iforest
 from eazieda.outliers_detect import outliers_detect_iqr
 from eazieda.outliers_detect import remove_outliers
 
+
 @pytest.fixture
 def hi_zscore_series():
-    l = [1] * 10
-    l.append(1e14)
-    return pd.Series(l)
+    list_vals = [1] * 10
+    list_vals.append(1e14)
+    return pd.Series(list_vals)
+
 
 @pytest.fixture
 def simple_series():
-    return pd.Series([1,2,1,2,1, 1000])
+    return pd.Series([1, 2, 1, 2, 1, 1000])
+
 
 @pytest.fixture
 def simple_series_and_array():
-    return (pd.Series([1,2,1,2,1, 1000]), np.array([False, False, False, False, False,  True]))
+    return (
+        pd.Series([1, 2, 1, 2, 1, 1000]),
+        np.array([False, False, False, False, False, True]),
+    )
+
 
 def test_outliers_detect_zscore(hi_zscore_series):
     """
@@ -30,9 +37,24 @@ def test_outliers_detect_zscore(hi_zscore_series):
 
     outliers = outliers_detect_zscore(s)
 
-    expected_output = np.array([False, False, False, False, False, False, False, False, False, False, True])
+    expected_output = np.array(
+        [
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            True,
+        ]
+    )
 
     assert np.array_equal(expected_output, outliers)
+
 
 def test_outliers_detect_iqr(simple_series):
     """
@@ -42,9 +64,10 @@ def test_outliers_detect_iqr(simple_series):
 
     outliers = outliers_detect_iqr(s)
 
-    expected_output = np.array([False, False, False, False, False,  True])
+    expected_output = np.array([False, False, False, False, False, True])
 
     assert np.array_equal(expected_output, outliers)
+
 
 def test_outliers_detect_iforest(simple_series):
     """
@@ -54,9 +77,10 @@ def test_outliers_detect_iforest(simple_series):
 
     outliers = outliers_detect_iforest(s)
 
-    expected_output = np.array([False, False, False, False, False,  True])
+    expected_output = np.array([False, False, False, False, False, True])
 
     assert np.array_equal(expected_output, outliers)
+
 
 def test_remove_outliers_inplace(simple_series_and_array):
     """
@@ -65,9 +89,10 @@ def test_remove_outliers_inplace(simple_series_and_array):
     s, outliers = simple_series_and_array
     remove_outliers(s, outliers, inplace=True)
 
-    expected_output = pd.Series([1,2,1,2,1])
+    expected_output = pd.Series([1, 2, 1, 2, 1])
 
     assert expected_output.equals(s)
+
 
 def test_remove_outlier_not_inplace(simple_series_and_array):
     """
@@ -76,8 +101,9 @@ def test_remove_outlier_not_inplace(simple_series_and_array):
     s, outliers = simple_series_and_array
     output = remove_outliers(s, outliers, inplace=False)
 
-    expected_output = pd.Series([1,2,1,2,1])
+    expected_output = pd.Series([1, 2, 1, 2, 1])
     assert expected_output.equals(output)
+
 
 def test_detect_outliers_integration_zscore_false(hi_zscore_series):
     """
@@ -85,10 +111,24 @@ def test_detect_outliers_integration_zscore_false(hi_zscore_series):
     """
     s = hi_zscore_series.copy()
     outliers = outliers_detect(s, method="zscore")
-    expected_output = np.array([False, False, False, False, False, False, False, False, False, False, True])
-    expected_s = hi_zscore_series
+    expected_output = np.array(
+        [
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            True,
+        ]
+    )
 
     assert np.array_equal(expected_output, outliers)
+
 
 def test_detect_outliers_integration_iqr_false(simple_series):
     """
@@ -96,12 +136,13 @@ def test_detect_outliers_integration_iqr_false(simple_series):
     """
     s = simple_series.copy()
     outliers = outliers_detect(s, method="iqr")
-    expected_output = np.array([False, False, False, False, False,  True])
+    expected_output = np.array([False, False, False, False, False, True])
     expected_s = simple_series
 
     assert np.array_equal(expected_output, outliers)
     # make sure nothing was removed
     assert expected_s.equals(s)
+
 
 def test_detect_outliers_integration_iforest_false(simple_series):
     """
@@ -109,12 +150,13 @@ def test_detect_outliers_integration_iforest_false(simple_series):
     """
     s = simple_series.copy()
     outliers = outliers_detect(s, method="iforest")
-    expected_output = np.array([False, False, False, False, False,  True])
+    expected_output = np.array([False, False, False, False, False, True])
     expected_s = simple_series
 
     assert np.array_equal(expected_output, outliers)
     # make sure nothing was removed
     assert expected_s.equals(s)
+
 
 def test_incorrect_input_detect():
     """
@@ -123,12 +165,14 @@ def test_incorrect_input_detect():
     with pytest.raises(TypeError):
         outliers_detect(12)
 
+
 def test_incorrect_method_detect(simple_series):
     """
     Error handling for wrong method input
     """
     with pytest.raises(ValueError):
-        outliers_detect(simple_series, method='test')
+        outliers_detect(simple_series, method="test")
+
 
 def test_incorrecttype_remove():
     """
@@ -136,6 +180,7 @@ def test_incorrecttype_remove():
     """
     with pytest.raises(TypeError):
         remove_outliers(12, np.array([True]))
+
 
 def test_incorrect_outliers_type_remove(simple_series):
     """
